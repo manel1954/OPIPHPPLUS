262550ef6507b42d25883d80996d358476784c41
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-05-11 18:50:17 +0200

    1

--- actualizar.sh
+++ actualizar.sh
@@ -114,7 +114,5 @@ frbm=`sed -n '13p'  $usuario/MMDVMHost/MMDVMHost.ini`
 frplus=`sed -n '13p'  $usuario/MMDVMHost/MMDVMHost.ini`
 sudo wget -post-data http://associacioader.com/prueba1.php?callBM=$bm'&'callPLUS=$plus'&'masterBM=$masterbm'&'masterPLUS=$masterplus'&'radio=$masterradio'&'version=$version'&'ESPECIAL=$masterespecial'&'YSFGateway=$masterYSFGateway                  
 
-sudo rm -R /home/pi/associacioader.com
-sudo rm -R /home/pi/A108/associacioader.com
-sudo rm /home/pi/Desktop/st-data
+
  
