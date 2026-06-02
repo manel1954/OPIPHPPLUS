3f0be0e3cc64b110a3720ea419c128c6e4fd1217
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-06-02 13:00:51 +0200

    1

--- actualizar_ids.sh
+++ actualizar_ids.sh
@@ -6,7 +6,7 @@
                         #cp DMRIds.dat /home/pi/MMDVMHost/
                         sudo chmod 777 /home/pi/MMDVMHost/DMRIds.dat
                         echo "*********************************************"
-                        echo "         IDS actualizados correctamente"
+                        echo "         IDS actualizados correctamentexxxxxxs"
                         echo "*********************************************"
                         sleep 4
 
